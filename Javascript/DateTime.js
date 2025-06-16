353b3148ce833f8f413e18231b486132706a65b8
BagdaSandip <sbagda051@gmail.com>
2025-06-16 22:00:18 +0530

    DateTime in JS

---
+++ Javascript/DateTime.js
@@ -0,0 +1,12 @@
+let mydate = new Date()
+console.log(typeof mydate);
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toTimeString());
+
+let Anotherdate = new Date(2023,0,5,23,5,3)
+console.log(Anotherdate.toLocaleString());
+let newDate = new   Date("26-03-2006");
+console.log(newDate.toLocaleString());
